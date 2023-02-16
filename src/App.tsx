753258be37ccea8fa8f42e958580f82114da8e1c
jillsuecamp <jillsue@udel.edu>
2023-02-16 01:14:33 -0500

    Completed the first 5 tasks successfully, with all tests passing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import "./App.css";
+import peanut from "./peanut.jpg";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                <h1> Jillian's Web Application</h1>
+                <h1> UDCIS CISC275 </h1>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. This is Jillian Camp.
             </p>
+            <img
+                src={peanut}
+                alt="My cat peanut"
+                width="500"
+                height="600"
+            ></img>
+            <ul>
+                <li>First thing</li>
+                <li>Another thing</li>
+                <li>A third item</li>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </ul>
         </div>
     );
 }
