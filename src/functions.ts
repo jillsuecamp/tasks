895ad83b1491713fc58f871e5543b7afa687f7a6
jillsuecamp <jillsue@udel.edu>
2023-02-22 22:25:04 -0500

    Finished all the functions and they all appear to pass

--- src/functions.ts
+++ src/functions.ts
@@ -42,7 +42,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.substr(-1) == "?" ? true : false;
 }
 
 /**
@@ -51,5 +51,17 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    return word == "yes"
+        ? true
+        : word == "Yes"
+        ? true
+        : word == "YES"
+        ? true
+        : word == "NO"
+        ? false
+        : word == "no"
+        ? false
+        : word == "No"
+        ? false
+        : null;
 }
