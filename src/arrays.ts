7a0f1e41f834b3d8cb866ac08eaa60cbe9a0f73f
Jillian Camp <jillsue@udel.edu>
2023-03-03 21:40:10 -0500

    finished tripleNumbers

--- src/arrays.ts
+++ src/arrays.ts
@@ -21,7 +21,10 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const numbersTripled: number[] = numbers.map(
+        (num: number): number => num * 3
+    );
+    return numbersTripled;
 }
 
 /**
