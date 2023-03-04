6de7b68f99a78ce15da4f507189aa3bd87ca2ee5
Jillian Camp <jillsue@udel.edu>
2023-03-04 15:20:06 -0500

    Finished countShortWords

--- src/arrays.ts
+++ src/arrays.ts
@@ -75,7 +75,8 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const onlyShort = words.filter((word: string): boolean => word.length < 4);
+    return onlyShort.length;
 }
 
 /**
