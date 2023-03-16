99715453131cee3644f1301d5b663c0f0c141f59
Jillian Camp <jillsue@udel.edu>
2023-03-16 00:45:00 -0400

    Finished RevealAnswer.

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,15 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [answer, setAnswer] = useState<boolean>(false);
+
+    function flipVisibility(): void {
+        setAnswer(!answer);
+    }
+    return (
+        <div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {answer && <div>42</div>}
+        </div>
+    );
 }
